78033106fcaed6e3afbc3803467d4175c0382f84
Linnia Hawkins <linnia@derecho3.hsn.de.hpc.ucar.edu>
2025-09-10 16:42:00 -0600

    create ptf functional test

--- testing/load_functional_tests.py
+++ testing/load_functional_tests.py
@@ -7,3 +7,5 @@ from functional_testing.fire.fuel.fuel_test import FuelTest
 from functional_testing.fire.ros.ros_test import ROSTest  
 from functional_testing.patch.patch_test import PatchTest
 from functional_testing.ml_phenology.phen_test import PhenologyTest
+from functional_testing.ml_ptf.ptf_test import PTFTest
+
